d856f9cd88acad4986214c783c77f1e49bed1463
Matthew Alton <matthew.alton@gmail.com>
2019-10-26 17:55:53 -0500

    Added tokens to the lexer.

--- src/cqcli/lex.h
+++ src/cqcli/lex.h
@@ -23,19 +23,28 @@
 #define TOK_ADD      1
 #define TOK_ALL      2
 #define TOK_ALTER    3
-#define TOK_CASCADE  4
-#define TOK_CREATE   5
-#define TOK_DISPLAY  6
-#define TOK_DROP     7
-#define TOK_GROUP    8
-#define TOK_OFFLINE  9
-#define TOK_ONLINE  10
-#define TOK_QUEUE   11
-#define TOK_REMOVE  12
-#define TOK_SET     13
-#define TOK_SOURCE  14
-#define TOK_SYSTEM  15
-#define TOK_TARGET  16
+#define TOK_BOOLEAN  4
+#define TOK_CASCADE  5
+#define TOK_CONNECT  6
+#define TOK_CREATE   7
+#define TOK_DISCNCT  8
+#define TOK_DISPLAY  9
+#define TOK_DROP    10
+#define TOK_END     11
+#define TOK_GROUP   12
+#define TOK_INTEGER 13
+#define TOK_NULL    14
+#define TOK_OFFLINE 15
+#define TOK_ONLINE  16
+#define TOK_QUEUE   17
+#define TOK_REMOVE  18
+#define TOK_SEMICLN 19
+#define TOK_SESSION 20
+#define TOK_SET     21
+#define TOK_SOURCE  22
+#define TOK_STRING  23
+#define TOK_SYSTEM  24
+#define TOK_TARGET  25
 
 #endif  /*  !defined _LEX_H  */
 
