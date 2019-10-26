62b79d5228b5adac7895d02736c0e6e8bda61663
Matthew Alton <matthew.alton@gmail.com>
2019-10-26 10:33:25 -0500

    Added lex.[ch] to cqcli.

---
+++ src/cqcli/lex.h
@@ -0,0 +1,41 @@
+
+/*
+ * Copyright (C) 2011,2012,2019 Matthew Alton
+ *
+ * This file is part of cURLiQ.
+ *
+ * cURLiQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cURLiQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cURLiQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LEX_H
+
+#define TOK_ADD      1
+#define TOK_ALL      2
+#define TOK_ALTER    3
+#define TOK_CASCADE  4
+#define TOK_CREATE   5
+#define TOK_DISPLAY  6
+#define TOK_DROP     7
+#define TOK_GROUP    8
+#define TOK_OFFLINE  9
+#define TOK_ONLINE  10
+#define TOK_QUEUE   11
+#define TOK_REMOVE  12
+#define TOK_SET     13
+#define TOK_SOURCE  14
+#define TOK_SYSTEM  15
+#define TOK_TARGET  16
+
+#endif  /*  !defined _LEX_H  */
+
