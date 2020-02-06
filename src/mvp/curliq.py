fed17a29c5824bbe2119171ada338e65456534f9
Matthew Alton <matthew.alton@gmail.com>
2020-02-06 22:34:30 +0000

    Added MVP

---
+++ src/mvp/curliq.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import yaml
+
+
+class System:
+    '''cURLiQ system object.'''
+
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
