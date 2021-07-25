56810e257853f6dd51906c06cb422458fac9a436
Matthew Alton <matthew.alton@gmail.com>
2021-07-24 22:33:48 -0500

    Initial commit of the C cURLiQ program.

---
+++ src/c/copyleft.c
@@ -0,0 +1,41 @@
+
+//
+//  Copyright (C) 2021 Matthew Alton
+//
+//  This file is part of cURLiQ.
+//
+//  cURLiQ is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  cURLiQ is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with cURLiQ.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <stdio.h>
+
+void
+copyleft(void)
+{
+ printf("Copyright (C) 2021 Matthew Alton\n");
+ printf("This file is part of cURLiQ.\n");
+ printf("cURLiQ is free software: you can redistribute it and/or modify\n");
+ printf(
+     "it under the terms of the GNU General Public License as published by\n");
+ printf("the Free Software Foundation, either version 3 of the License, or\n");
+ printf("(at your option) any later version.\n");
+ printf("cURLiQ is distributed in the hope that it will be useful,\n");
+ printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+ printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+ printf("GNU General Public License for more details.\n");
+ printf("You should have received a copy of the GNU General Public License\n");
+ printf("along with cURLiQ.  If not, see <http://www.gnu.org/licenses/>.\n");
+
+ return;
+}
---
+++ src/c/curliq.h
@@ -0,0 +1,29 @@
+
+//
+//  Copyright (C) 2021 Matthew Alton
+//
+//  This file is part of cURLiQ.
+//
+//  cURLiQ is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  cURLiQ is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with cURLiQ.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _CURLIQ_H
+
+#define _MISO_H 1
+
+#define CURLIQ_VERSION_MAJOR 0
+#define CURLIQ_VERSION_MINOR 0
+#define CURLIQ_VERSION_PATCH 0
+
+#endif // !defined _CURLIQ_H
